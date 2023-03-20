eb8f3c94811a513d1a9308364577156d321217cc
joethyr <joethayr@gmail.com>
2023-03-19 18:34:18 -0600

    add css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="calculator-grid">
-      <div className="output"></div>
+      <div className="output">
+        <div className="previous-operand">123,345</div>
+        <div className="current-operand">987,000</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button className="">DEL</button>
+      <button className="">/</button>
+      <button className="">1</button>
+      <button className="">2</button>
+      <button className="">3</button>
+      <button className="">*</button>
+      <button className="">4</button>
+      <button className="">5</button>
+      <button className="">6</button>
+      <button className="">+</button>
+      <button className="">7</button>
+      <button className="">8</button>
+      <button className="">9</button>
+      <button className="">-</button>
+      <button className="">.</button>
+      <button className="">0</button>
+      <button className="span-two">=</button>
     </div>
-    );
+  );
 }
 
 export default App;
